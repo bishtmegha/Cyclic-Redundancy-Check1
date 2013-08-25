bf26a4a6d66febf5a5aaa21112170c145fba0923
Megha Bisht <bishtmegha916@gmail.com>
2013-08-25 22:03:51 +0530

    Update crc.py

--- workspace/my progs/CRC.py/crc.py
+++ workspace/my progs/CRC.py/crc.py
@@ -5,7 +5,7 @@ print "Enter the genrator bit string: ",
 GeneString = raw_input()
 for i in xrange(0, len(GeneString)):
     BitString = BitString + '0'
-print "Appended bitstring: ", BitString
+print "Appended bitstring: "+ BitString
 quo = BitString
 for i in xrange(0,length):
     if (BitString[i] == '0'):
@@ -20,4 +20,4 @@ for i in xrange(0,length):
 for i in xrange(length ,length + len(GeneString)-1):
     print "Transmitted String is :"+BitString
 for i in xrange(length,length+length+len(GeneString)-1):
-    print quo
+    print "CRC:" +quo
