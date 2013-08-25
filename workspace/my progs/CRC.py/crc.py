46c134be5928582ddb2884eb122eee7e0b08dba9
megha <bishtmegha916@gmail.com>
2013-08-25 21:10:04 +0530

    2nd commit

--- workspace/my progs/CRC.py/crc.py
+++ workspace/my progs/CRC.py/crc.py
@@ -5,20 +5,20 @@ print "enter the genrator bit string",
 GeneString = raw_input()
 for i in xrange(0, len(GeneString)):
     BitString = BitString + '0'
-print BitString
+print "appended bitstring :"+BitString
 quo = BitString
 for i in xrange(0,length):
     if (BitString[i] == '0'):
          for j in xrange(i,len(GeneString +1)):
-             BitString[j]=BitString[j]^0
+             BitString[j]=str(int(BitString[j]))^0
     else:
-        BitString[i] = BitString[i]^GeneString[0]
-        BitString[i+1]= BitString[i+1]^GeneString[1]         
-        BitString[i+2]= BitString[i+2]^GeneString[2]
-        BitString[i+3]= BitString[i+3]^GeneString[3]
+         BitString[i] = str(int(BitString[i])^int(GeneString[0]))
+         BitString[i+1]= str(int(BitString[i+1])^int(GeneString[1]))       
+         BitString[i+2]= str(int(BitString[i+2])^int(GeneString[2]))
+         BitString[i+3]= str(int(BitString[i+3])^int(GeneString[3]))
         
 for i in xrange(length ,length + len(GeneString)-1):
-    print "Transmitted String is :"
+    print "Transmitted String is :"+BitString
 for i in xrange(length,length+length+len(GeneString)-1):
     print quo     
         
