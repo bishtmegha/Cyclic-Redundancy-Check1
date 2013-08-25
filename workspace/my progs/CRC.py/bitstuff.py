1a65ccce0af354b9a08b5af9870655b8e1ef9c78
Megha Bisht <bishtmegha916@gmail.com>
2013-08-25 21:55:12 +0530

    Update bitstuff.py

--- workspace/my progs/CRC.py/bitstuff.py
+++ workspace/my progs/CRC.py/bitstuff.py
@@ -1,6 +1,6 @@
 input, i = raw_input('Enter bit stream:\n'), 0;
 while 1:
-	i = input.find('00000',i)
+	i = input.find('11111',i)
 	if i == -1: break
-	input, i = input[:i+5] + '1' + input[i+5:], i + 4
+	input, i = input[:i+5] + '0' + input[i+5:], i + 4
 print input
